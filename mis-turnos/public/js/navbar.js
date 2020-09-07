c5c6a95f599b0d6783ccd6c14ccd97b503bd29f6
Mateo <maferretto@oadearl.com.ar>
2020-09-07 18:04:02 -0300

    Mis queridos tabs

--- mis-turnos/public/js/navbar.js
+++ mis-turnos/public/js/navbar.js
@@ -10,24 +10,24 @@ var sticky = navbar.offsetTop;
 
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
 function stickyHeader() {
-    if (window.pageYOffset >= sticky) {
-        navbar.classList.add("sticky"); 
-        pedirTurno.classList.add("d-none");
-        pedirTurno.classList.add("d-lg-block");
-        medio.classList.add("margen");
-    } else {
-        navbar.classList.remove("sticky");
-        pedirTurno.classList.remove("d-none");
-        medio.classList.remove("margen");
-    }
+	if (window.pageYOffset >= sticky) {
+		navbar.classList.add("sticky"); 
+		pedirTurno.classList.add("d-none");
+		pedirTurno.classList.add("d-lg-block");
+		medio.classList.add("margen");
+	} else {
+		navbar.classList.remove("sticky");
+		pedirTurno.classList.remove("d-none");
+		medio.classList.remove("margen");
+	}
 }
 
 function openNav() {
-    document.getElementById("mySidebar").style.width = "250px";
-    document.getElementById("main").style.marginLeft = "250px";
-  }
-  
+	document.getElementById("mySidebar").style.width = "250px";
+	document.getElementById("main").style.marginLeft = "250px";
+}
+
 function closeNav() {
-    document.getElementById("mySidebar").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
+	document.getElementById("mySidebar").style.width = "0";
+	document.getElementById("main").style.marginLeft= "0";
 }
